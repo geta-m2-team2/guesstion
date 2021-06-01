87bae2b41d2af8b16279ffc74ed8e663668500ed
BluABK <bluabk@bluabk.net>
2021-06-01 14:38:05 +0200

    Added developer menu with page goto

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,20 @@ function updateViews() {
             ${renderCurrentPageView()}
         </div>
     `;
+
+    // FIXME: Temporary development manual page navigation
+    document.getElementById("devel-nav").innerHTML = `   
+      <label for="page-nav">[DEVEL] Goto page: </label>
+      <select name="Page" id="page-nav">
+        <option value="Home" onClick="goToPage('home')" ${model.currentPage === "home" ? 'selected="selected"' : ""}>Home</option>
+        <option value="Lobby" onClick="goToPage('lobby')" ${model.currentPage === "lobby" ? 'selected="selected"' : ""}>Lobby</option>
+      </select>
+    `;
+}
+
+function goToPage(pageName) {
+    model.currentPage = pageName;
+    updateViews();
 }
 
 function renderErrorPageView(errorCode, page) {
